de685e4bf18a91467bb44f4ced512361ed2b0c09
CJLorax <casey.jones@tstc.edu>
2015-12-23 16:41:35 -0600

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
-<<<<<<< HEAD
-this is a test
-=======
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf340
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+#include <iostream> 
 
-\f0\fs24 \cf0 int main()\{\
-\
-\
-return 0;\
-\
-\}}
->>>>>>> origin/master
+using namespace std;
+
+int main() {
+
+#if defined (_WIN32) ||  (_WIN64) 
+
+	cout << "This is Windows" << endl;
+
+#endif
+
+#if defined (__APPLE__) 
+
+	cout << "This is Apple" << endl;
+
+#endif
+
+	//test link
+	cin.get();
+	return 1;
+}
