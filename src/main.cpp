083b4606f74fc7c6ed7c44e2df7a3db87787e8c6
CJLorax <cjones@Caseys-iMac.local>
2015-12-23 17:07:51 -0600

    Added from mac

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,34 @@
-#include <iostream> 
+#include <stdio.h>
+
+#include <iostream>
 
 using namespace std;
 
-int main() {
-
-#if defined (_WIN32) ||  (_WIN64) 
-
-	cout << "This is Windows" << endl;
-
+int main(int argc, const char * argv[]) {
+    
+#if defined (_WIN32) ||  (_WIN64)
+    
+    cout << "This is Windows" << endl;
+    cout << "Would this show in windows" << endl;
+    
 #endif
-
-#if defined (__APPLE__) 
-
-	cout << "This is Apple" << endl;
-
+    
+#if defined (__APPLE__)
+    
+    cout << "This is Apple" << endl;
+    cout << "Does this add more???" << endl;
+    cout << "AND Does this add more???" << endl;
+    
+    
 #endif
-
-#if defined (_linux__) 
-
-	cout << "This is Linux" << endl;
-
+    
+#if defined (_linux__)
+    
+    cout << "This is Linux" << endl;
+    
 #endif
-
-	//test link
-	cin.get();
-	return 1;
+    
+    //test link
+    cin.get();
+    return 1;
 }
