56e617dd8d2569333d3d2d568d4f44e8d7f5dda1
CJLorax <casey.jones@tstc.edu>
2015-12-24 15:49:49 -0600

    working on cross platform directorys

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,33 @@
 #include <stdio.h>
+#include <string>
+#include <iostream>
+
 
 #if defined (_WIN32) ||  (_WIN64)
 #include <direct.h>
-#define GetCurrentDir _getcwd
+#define getcwd _getcwd
 #endif
 
 
 #if defined (__linux__)
 #include <unistd.h>
-#include <limits.h>
-#define GetCurrentDir getcwd
 #endif
 
-#include <iostream>
+
 
 using namespace std;
+
 
 
 int main(int argc, const char * argv[]) {
     
 #if defined (_WIN32) ||  (_WIN64)
-    
+	cout << "This is Windows" << endl;
+	
+	string s_cwd(getcwd(NULL, 0));
 
-	char cCurrentPath[FILENAME_MAX];
+	cout << s_cwd << endl;
 
-	cCurrentPath[sizeof(cCurrentPath) - 1] = '/0'; /* not really required */
-
-	//printf("The current working directory is %s", cCurrentPath);
-
-	cout << "The current working directory is " + cCurrentPath << endl;
-    
 #endif
     
 #if defined (__APPLE__)
@@ -43,11 +41,10 @@ int main(int argc, const char * argv[]) {
 #if defined (__linux__)
     
     cout << "This is Linux" << endl;
+	
+	string s_cwd(getcwd(NULL, 0));
 
-	char result[PATH_MAX];
-	string test = getcwd(result, PATH_MAX);
-
-	cout << "The current working directory is " + test << endl;
+	cout << s_cwd << endl;
 
 #endif
     
